f513cf53bef1b1b4f4587f3f692f470efd86409e
loginshin <95666408+loginshin@users.noreply.github.com>
2024-01-08 19:03:12 +0900

    [level unrated] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String result ="";
+        result = my_string.repeat(k);
+        String answer = result;
+
+
+        return answer;
+    }
+}
