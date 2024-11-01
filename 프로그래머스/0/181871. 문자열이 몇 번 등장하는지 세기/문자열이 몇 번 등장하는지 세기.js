9ff7d013904f3042a124171f341a92110efcc8ad
LoGinShin <95666408+loginshin@users.noreply.github.com>
2024-11-01 17:42:32 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 18.06 ms, Memory: 34.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.js
@@ -0,0 +1,27 @@
+function solution(myString, pat) {
+  let answer = 0;
+    let myStringCharArr = myString.split('');
+    let patCharArr = pat.split('');
+    let chkCount = 0;
+
+    for (let i = 0; i < myStringCharArr.length; i++) {
+        if (myStringCharArr[i] === patCharArr[chkCount]) {
+
+            console.log(myStringCharArr[i], i);
+
+            for (let l = 0; l < pat.length; l++) {
+                if (patCharArr[l] === myStringCharArr[i + l] ) {
+                    console.log(patCharArr[l]);
+                    console.log(myStringCharArr[i + l]);
+                    chkCount++;
+                }
+            }
+            if (chkCount === pat.length) {
+                answer++;
+            }
+        }
+        chkCount = 0;
+    }
+
+    return answer;
+}
