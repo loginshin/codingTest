90b808e773ecd37b90c048865d614b0c38fc1f60
LoGinShin <95666408+loginshin@users.noreply.github.com>
2024-11-04 16:28:43 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,11 +1,3 @@
 function solution(array, n) {
-   let answer = 0;
-
-    const arrayMap = array.map( function(value, index, array) {
-        if ( value === n ) {
-            answer++;
-        }
-    });
-
-    return answer;
+    return array.filter((data) => data === n).length;
 }
