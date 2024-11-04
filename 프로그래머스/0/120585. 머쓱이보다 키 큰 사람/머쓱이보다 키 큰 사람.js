57756092c524f4c6a32228fae1d765fe75571d02
LoGinShin <95666408+loginshin@users.noreply.github.com>
2024-11-04 16:42:46 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,10 +1,5 @@
 function solution(array, height) {
-       let answer = 0;
-
-    array.map(function(value) {
-        if ( value > height ) {
-            answer++;
-        }
-    });
+    let answer = 0;
+    array.map((arr) => height < arr && answer++);
     return answer;
 }
